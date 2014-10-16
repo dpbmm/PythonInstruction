ebfdb8fdc8fe54b8a472deab76e3d3af7e314571
David Branner <databranner@gmail.com>
2014-10-15 21:19:14 -0400

    add dubious_sort.py

---
+++ dubious_sort.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+# dubious_sort.py
+# 20141015
+# David Prager Branner
+
+"""Implement quicksort, but if broken=True then do not sort correctly."""
+
+import random
+
+def quicksort(the_list, broken=False):
+    '''Quicksort: choose pivot, divide as > or <= pivot; call recursively.'''
+    # Recursion returns
+    if not the_list:
+        return the_list
+    #
+    # Divide, using the_list[0] as pivot
+    # Note: no particular speed improvement using array.array instead of list.
+    first_half = []
+    second_half = []
+    for item in the_list[1:]:
+        if item < the_list[0]:
+            first_half.append(item)
+        else:
+            second_half.append(item)
+    #
+    # Recursive call and recombination
+    if broken:
+        # Wreck the sort
+        random.shuffle(the_list)
+        return the_list
+    return (quicksort(first_half, broken) +
+            list([the_list[0]]) +
+            quicksort(second_half, broken))
